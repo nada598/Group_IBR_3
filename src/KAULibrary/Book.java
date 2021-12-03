3247edac5c77f2d39dd06278efdee86316582470
DELL <DELL@192.168.1.106>
2021-12-03 21:27:07 +0300

    commitH

--- src/KAULibrary/Book.java
+++ src/KAULibrary/Book.java
@@ -323,20 +323,8 @@ public class Book {
         }
         return "";
     }
-    // a method that allows the user to cancle a borrowing process
-    public void borrowCancelation(String isbn, String userID) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String ConnectionURL = "jdbc:mysql://localhost:3306/KAULibraryDB";
-            Connection con = DriverManager.getConnection(ConnectionURL, "root", "root");
-            Statement st = con.createStatement();
-            st.executeUpdate("delete from issued_book where isbn= " + isbn + " AND userID = '" + userID + "'");
-            increaseBookAvailability(isbn);
-        } catch (Exception e) {
-            System.out.println("SQL statement is not executed!");
-            System.err.println(e.getMessage());
-        }
-    }
+  
+    
     // a method to display all the issued books of the user on a table
     public void populateUserTabel(JTable borrowedBook, String id) {
         DefaultTableModel userTabel = (DefaultTableModel) borrowedBook.getModel();
@@ -423,28 +411,7 @@ public class Book {
         return "";
     }
 
-    // a method to check the copied of the book
-    public String checkBook(String isbn) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String ConnectionURL = "jdbc:mysql://localhost:3306/KAULibraryDB";
-            con = DriverManager.getConnection(ConnectionURL, "root", "root");
-
-            String selectSQL = "SELECT availability FROM book WHERE ISBN = '" + isbn + "'";
-            PreparedStatement preparedStmt = con.prepareStatement(selectSQL);
-            ResultSet stmtResult = preparedStmt.executeQuery();
-            stmtResult.next();
-            if (stmtResult.getInt("availability") > 0) {
-                return "";
-            }
-            con.close();
-        } catch (SQLException s) {
-            System.out.println("SQL statement is not executed!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "availability";
-    }
+    
     
     // a method to decrease the number of the book copied if a student borrowed a copy
     public void decreaseBookAvailability(String isbn) {
@@ -469,33 +436,6 @@ public class Book {
         }
     }
 
-    // a method to check if the user has borrowed more than 5 books if so their status become false
-    public String checkUserHistory(String id) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            String ConnectionURL = "jdbc:mysql://localhost:3306/KAULibraryDB";
-            con = DriverManager.getConnection(ConnectionURL, "root", "root");
-
-            String selectSQL = "SELECT userID, COUNT(userID) FROM issued_book WHERE userID = '" + id + "'";
-            PreparedStatement preparedStmt = con.prepareStatement(selectSQL);
-            ResultSet stmtResult = preparedStmt.executeQuery();
-            stmtResult.next();
-            if (stmtResult.getInt("count(userID)") < 5) {
-                return "";
-            } else {
-                String query = "update user set status = ? WHERE id = '" + id + "'";
-                PreparedStatement preparedStmt2 = con.prepareStatement(query);
-                preparedStmt2.setBoolean(1, false);
-                preparedStmt2.executeUpdate();
-            }
-            con.close();
-        } catch (SQLException s) {
-            System.out.println(s + " checkUserHistory");
-            System.out.println("SQL statement is not executed!");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return "history";
-    }
+  
     
 }//class book
