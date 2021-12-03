6719b04e087cd01fe01a50d17c0ad78c843debe1
HP <HP@DESKTOP-116UI9B>
2021-12-03 18:37:19 +0300

    NADA2

--- src/KAULibrary/Book.java
+++ src/KAULibrary/Book.java
@@ -323,5 +323,179 @@ public class Book {
         }
         return "";
     }
+    // a method that allows the user to cancle a borrowing process
+    public void borrowCancelation(String isbn, String userID) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String ConnectionURL = "jdbc:mysql://localhost:3306/KAULibraryDB";
+            Connection con = DriverManager.getConnection(ConnectionURL, "root", "root");
+            Statement st = con.createStatement();
+            st.executeUpdate("delete from issued_book where isbn= " + isbn + " AND userID = '" + userID + "'");
+            increaseBookAvailability(isbn);
+        } catch (Exception e) {
+            System.out.println("SQL statement is not executed!");
+            System.err.println(e.getMessage());
+        }
+    }
+    // a method to display all the issued books of the user on a table
+    public void populateUserTabel(JTable borrowedBook, String id) {
+        DefaultTableModel userTabel = (DefaultTableModel) borrowedBook.getModel();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String ConnectionURL = "jdbc:mysql://localhost:3306/KAULibraryDB";
+            Connection con = DriverManager.getConnection(ConnectionURL, "root", "root");
+
+            String selectSQL = "SELECT ISBN, return_date, due_date, borrowed_date FROM issued_book where userID = '" + id + "'";
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(selectSQL);
+            while (rs.next()) {
+                String[] row = {rs.getString("ISBN"), rs.getString("return_date"), rs.getString("due_date"), rs.getString("borrowed_date")};
+                userTabel.addRow(row);
+            }
+            st.close();
+        } catch (Exception e) {
+            System.out.println("SQL statement is not executed!");
+            System.err.println(e.getMessage());
+        }
+    }
+    
+    public String issueBook(String userID, String isbn) {
+        // a call for a method that check if the book has avalilable copies 
+        String bookAvailability = checkBook(isbn);
+        /* a call for a method that check if the user is allowed to borrow a book
+         user is not allowed to borrow a book if there is 5 issued books to their profile if they has 5 books their status become false */
+        String userHistory = checkUserHistory(userID);
+        if (bookAvailability.equalsIgnoreCase("") && userHistory.equalsIgnoreCase("")) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                String ConnectionURL = "jdbc:mysql://localhost:3306/KAULibraryDB";
+                con = DriverManager.getConnection(ConnectionURL, "root", "root");
+                
+                // a query to get the user id and status
+                String selectSQL = "SELECT status, id FROM user WHERE Id = '" + userID + "'";
+                PreparedStatement preparedStmt = con.prepareStatement(selectSQL);
+                ResultSet stmtResult = preparedStmt.executeQuery();
+                
+                // a query to get the book isbn
+                String selectSQL2 = "SELECT ISBN, availability FROM book WHERE isbn = '" + isbn + "'";
+                PreparedStatement preparedStmt2 = con.prepareStatement(selectSQL2);
+                ResultSet stmtResult2 = preparedStmt2.executeQuery();
+                
+                // get the system(util) date and convert it to sql date 
+                Calendar c = Calendar.getInstance();
+                java.util.Date currentDate = new Date();
+                java.sql.Date sqlBorrowDate = new java.sql.Date(currentDate.getTime());
+                c.setTime(currentDate);
+                // caculate the 5 days after the borrowing date to return the book
+                c.add(Calendar.DAY_OF_MONTH, 5);
+                java.sql.Date sqlDueDate = new java.sql.Date(c.getTimeInMillis());
+
+                stmtResult.next();
+                stmtResult2.next();
+                
+                if (stmtResult.getBoolean("status")) {
+                    
+                    // a prepared query to fill the issued book table with the user id, book isbn, due date to return, borrowed date
+                    String query = " insert into issued_book (userID, ISBN, borrowed_date, due_date)"
+                            + " values (?, ?, ?, ?)";
+                    PreparedStatement preparedStmt3 = con.prepareStatement(query);
+                    preparedStmt3.setString(1, stmtResult.getString("id"));
+                    preparedStmt3.setString(2, stmtResult2.getString("ISBN"));
+                    preparedStmt3.setDate(3, sqlBorrowDate);
+                    preparedStmt3.setDate(4, sqlDueDate);
+                    preparedStmt3.execute();
+                    // a call of method to decrease the copied of the book
+                    decreaseBookAvailability(isbn);
+                }
+                con.close();
+                System.out.println("successful");
+                return "successful";
+            } catch (SQLException s) {
+                System.out.println("SQL statement is not executed!");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (bookAvailability.equalsIgnoreCase("availability")) {
+            return "availability";
+        } else if (userHistory.equalsIgnoreCase("history")) {
+            return "history";
+        }
+        return "";
+    }
+
+    // a method to check the copied of the book
+    public String checkBook(String isbn) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String ConnectionURL = "jdbc:mysql://localhost:3306/KAULibraryDB";
+            con = DriverManager.getConnection(ConnectionURL, "root", "root");
+
+            String selectSQL = "SELECT availability FROM book WHERE ISBN = '" + isbn + "'";
+            PreparedStatement preparedStmt = con.prepareStatement(selectSQL);
+            ResultSet stmtResult = preparedStmt.executeQuery();
+            stmtResult.next();
+            if (stmtResult.getInt("availability") > 0) {
+                return "";
+            }
+            con.close();
+        } catch (SQLException s) {
+            System.out.println("SQL statement is not executed!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "availability";
+    }
+    
+    // a method to decrease the number of the book copied if a student borrowed a copy
+    public void decreaseBookAvailability(String isbn) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String ConnectionURL = "jdbc:mysql://localhost:3306/KAULibraryDB";
+            con = DriverManager.getConnection(ConnectionURL, "root", "root");
+
+            String selectSQL = "SELECT availability FROM book WHERE isbn = '" + isbn + "'";
+            PreparedStatement preparedStmt = con.prepareStatement(selectSQL);
+            ResultSet stmtResult = preparedStmt.executeQuery();
+            stmtResult.next();
+            String query = "update book set availability = ? WHERE isbn = '" + isbn + "'";
+            PreparedStatement preparedStmt2 = con.prepareStatement(query);
+            preparedStmt2.setInt(1, stmtResult.getInt("availability") - 1);
+            preparedStmt2.executeUpdate();
+            con.close();
+        } catch (SQLException s) {
+            System.out.println("SQL statement is not executed!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // a method to check if the user has borrowed more than 5 books if so their status become false
+    public String checkUserHistory(String id) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String ConnectionURL = "jdbc:mysql://localhost:3306/KAULibraryDB";
+            con = DriverManager.getConnection(ConnectionURL, "root", "root");
+
+            String selectSQL = "SELECT userID, COUNT(userID) FROM issued_book WHERE userID = '" + id + "'";
+            PreparedStatement preparedStmt = con.prepareStatement(selectSQL);
+            ResultSet stmtResult = preparedStmt.executeQuery();
+            stmtResult.next();
+            if (stmtResult.getInt("count(userID)") < 5) {
+                return "";
+            } else {
+                String query = "update user set status = ? WHERE id = '" + id + "'";
+                PreparedStatement preparedStmt2 = con.prepareStatement(query);
+                preparedStmt2.setBoolean(1, false);
+                preparedStmt2.executeUpdate();
+            }
+            con.close();
+        } catch (SQLException s) {
+            System.out.println(s + " checkUserHistory");
+            System.out.println("SQL statement is not executed!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "history";
+    }
     
 }//class book
